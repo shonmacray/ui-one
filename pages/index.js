b90176bdced9d5f086bbe0e32688c12c76344f26
shon macray <shonmacray@gmail.com>
2020-07-12 11:35:26 +0300

    created nice background

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>ui one</title>
         <link rel="icon" href="/favicon.ico" />
-        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
       </Head>
-
+      <div className="blur">
+       <div>
+         <h1>heh</h1>
+       </div>
+      </div>
     </div>
   )
 }
