368b62a6c37aa5517c380bc79fa1cd19592ef288
shon macray <shonmacray@gmail.com>
2020-07-14 14:02:05 +0300

    changed fonts

--- components/body.js
+++ components/body.js
@@ -4,8 +4,12 @@ import styles from "./body.module.css"
 export default function Body() {
     return (
         <div className={styles.body}>
-            <div className="container">
-                <h1>Loook</h1>
+            <div className="container-fluid">
+                <div className="d-flex pl-5 pr-2 pt-5">
+                    <div className="w-25 pl-3">My Cards <i className="fas fa-edit"/> </div>
+                    <div className="w-25 pl-3">Send Money</div>
+                    <div className="w-50 pl-3">All Transcations</div>
+                </div>
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
       <Head>
         <title>ui one</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous"></link>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous"></link>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossOrigin="anonymous"></link>
       </Head>
       <div className="blur">
        <Header/>
